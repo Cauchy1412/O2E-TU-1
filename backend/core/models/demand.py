47e00fb7df88f571d73b6f7de476887187842b0a
史默迪 <19373471@buaa.edu.cn>
2022-04-16 17:53:15 +0800

    demand.py

---
+++ backend/core/models/demand.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from .user import User
+from .user import User
+
+
+
+class Demand(models.Model):
+    """comment
+
+    Fields:
+        - interpretation: a foreignkey to interpretation
+        - user: a foreignkey to User
+        - created_at = created time
+        - text: comment content
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sended_comments")
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=500)
+    others = JSONField()
+
