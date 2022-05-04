73bb345c83f30f0d40ff8722660d3a93273bb9fc
杨淇文 <19373512@buaa.edu.cn>
2022-05-04 20:11:08 +0800

    订单详情和订单管理

---
+++ app/pages/Order/OrderDetail.vue
@@ -0,0 +1,399 @@
+<template>
+	<view>
+		<view class="container">
+			<tui-navigation-bar backgroundColor="255,255,255" :isFixed="false" :isOpcity="false">
+				<view class="tui-content-box">
+					<view class="tui-avatar-box">
+						<tui-icon name="back" color="#FFE933" :size="64"></tui-icon>
+					</view>
+					<view class="tui-search-box">
+						<view class="tui-search-text">订单详情</view>
+					</view>
+					<view class="tui-notice-box" v-on:click="addOrder()">
+						<text class="tui-add-text">返回</text>
+					</view>
+				</view>
+			</tui-navigation-bar>
+		</view>
+		<view class="select-topic-class">
+			<view class="select-title" style='width: 100%'>
+				<text>
+					{{Data.title}}
+				</text>
+			</view>
+		</view>
+		<view class="example">
+			<uni-forms :modelValue="formData">
+				<uni-forms-item label='订单编号'>
+					{{Data.order_id}}
+				</uni-forms-item>
+				<uni-forms-item label='发布公司'>
+					{{Data.Name}}
+				</uni-forms-item>
+				<uni-forms-item label='订单价格'>
+					{{Data.Price}}
+				</uni-forms-item>
+				<uni-forms-item label='订单时间'>
+					{{Data.Lasttime}}
+				</uni-forms-item>
+				<uni-forms-item label='订单状态'>
+					{{Data.state}}
+				</uni-forms-item>
+			</uni-forms>
+		</view>
+		<view v-if="userInfo.user_type">
+			<view class="order-info-detail-receive" v-on:click="addOrder()">
+				接受
+			</view>
+			<view class="order-info-detail-refuse" v-on:click="Refuse()">
+				拒绝
+			</view>
+		</view>
+		<view v-else="userInfo.user_type">
+			<view class="order-info-detail-Complete" v-on:click="Complete()">
+				完成订单
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {mapState, mapMutations} from 'vuex'
+	import {api} from '@/api'
+	export default {
+		data() {
+			return {
+				Data:{
+					user_state:'',
+					order_id:'',
+					title:'',
+					Detail:'',
+					Price:'',
+					Lasttime:'',
+					state:'',
+					Name:''
+				}
+			}
+		},
+		onLoad: function(option){
+			this.Data.order_id = option.id
+			let orderdata = this.getData()
+			this.Data.user_state = orderdata.state
+			this.Data.title = orderdata.title
+			this.Data.Price = orderdata.price
+			this.Data.Lasttime = orderdata.time
+		},
+		computed: {
+			...mapState(['userInfo'])
+		},
+		methods: {
+			async addOrder() {
+				console.log('aaa')
+				const id = 3
+				const price = 3
+				const time = 3
+				const rest = await api.post('resolution/create-order',{
+					id, time, price})
+			},
+			back:function(){
+				uni.navigateTo({
+					url:'./OrderManagement'
+				})
+			},
+			async getData(stringofid) {
+				let result = await api.get('resolution/'+stringofid)
+				return result
+			},
+			async Complete() {
+				if (this.state==1) {
+					change_state=2
+					const resp = await api.post('resolution/update-resolution-state', {
+						id:order_id,state:change_state
+					});
+					if (!resp) {
+						uni.navigateBack({
+							complete() {
+								setTimeout(() => {
+									uni.showToast({
+										title: '订单完成',
+										icon: "success",
+										duration: 1000
+									});
+								}, 100);
+							}
+						});
+					}
+					this.toast('完成失败');
+				}
+			},
+			async Accept (){
+				if (this.state==1) {
+					change_state=2
+					const resp = await api.post('resolution/update-resolution-state', {
+						id:order_id,state:change_state
+					});
+					if (!resp) {
+						uni.navigateBack({
+							complete() {
+								setTimeout(() => {
+									uni.showToast({
+										title: '接受成功',
+										icon: "success",
+										duration: 1000
+									});
+								}, 100);
+							}
+						});
+					}
+					this.toast('接受失败');
+				}
+			},
+			async Refuse(){
+				if (this.state==1) {
+					change_state=4
+					const resp = await api.post('resolution/update-resolution-state', {
+						id:order_id,state:change_state
+					});
+					if (!resp) {
+						uni.navigateBack({
+							complete() {
+								setTimeout(() => {
+									uni.showToast({
+										title: '拒绝成功',
+										icon: "success",
+										duration: 1000
+									});
+								}, 100);
+							}
+						});
+					}
+					this.toast('拒绝失败');
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	page {
+		background-color: #fff;
+
+	}
+
+	.container {
+		padding: 0upx 0 120upx 0;
+		box-sizing: border-box;
+		position: relative;
+	}
+	
+	.header {
+		padding: 80upx 90upx 60upx 90upx;
+		box-sizing: border-box;
+	}
+	
+	.title {
+		font-size: 34upx;
+		color: #333;
+		font-weight: 500;
+	}
+	
+	.sub-title {
+		font-size: 24upx;
+		color: #7a7a7a;
+		padding-top: 18upx;
+	}
+	
+	.tui-box-upload {
+		padding-left: 25upx;
+		margin-bottom: 90upx;
+		box-sizing: border-box;
+	}
+	
+	.tui-title {
+		width: 100%;
+		padding: 50upx 30upx 30upx;
+		box-sizing: border-box;
+		font-weight: bold;
+	}
+	
+	.tui-header-bg {
+		width: 100%;
+		margin: 0;
+	}
+	
+	.tui-header-img {
+		width: 100%;
+		height: 440upx;
+		display: block;
+	}
+	
+	.tui-header-icon {
+		width: 100%;
+		position: fixed;
+		top: 0;
+		padding: 0 12upx;
+		display: flex;
+		align-items: center;
+		height: 64upx;
+		transform: translateZ(0);
+		z-index: 99999;
+		box-sizing: border-box;
+	}
+	
+	.tui-content-box {
+		width: 100%;
+		height: 88upx;
+		padding: 0 30upx;
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	
+	.tui-avatar-box {
+		width: 60upx;
+		height: 60upx;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-shrink: 0;
+	}
+	
+	.tui-avatar {
+		width: 56upx;
+		height: 56upx;
+		border-radius: 50%;
+	}
+	
+	.tui-search-box {
+		height: 64upx;
+		margin: 0 28upx;
+		border-radius: 36upx;
+		font-size: 36upx;
+		padding: 0 24upx;
+		box-sizing: border-box;
+		color: #bfbfbf;
+		display: flex;
+		align-items: center;
+		font-weight: 700;
+		color: #000000;
+	}
+	
+	.select-topic-class {
+		height: 72upx;
+		margin: 20upx 20upx;
+		border-radius: 10upx;
+		background-color: #f1f1f1;
+		padding: 0 24upx;
+		box-sizing:border-box;
+		color: #bfbfbf;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	
+	.select-title {
+		color: #000000;
+		font-size: 34upx;
+		font-weight: 700;
+	}
+	.select-desc {
+		display: flex;
+		align-items: center;
+	}
+	.add-topic-text{
+		box-sizing: border-box;
+		width: 100%;
+		padding-top: 10upx;
+		padding-left: 45upx;
+	}
+	.tui-bg-white {
+		background-color: #ffffff !important;
+	}
+	
+	.tui-search-text {
+		padding-left: 10upx;
+	}
+	
+	.tui-add-text {
+		color: #000000;
+		padding: 10upx 30upx;
+		font-size: 28upx;
+		box-sizing: border-box;
+		font-weight: 700;
+		border-radius: 40upx;
+		background-color: #FFE933;
+	}
+	.topic-tilte{
+		padding: 10upx 40upx;
+		box-sizing: border-box;
+		font-weight: 700;
+		color:#5677fc;
+	}
+	.tui-notice-box {
+		position: relative;
+		flex-shrink: 0;
+		font-size: 44upx;
+		color: #fff;
+	}
+	
+		.example {
+			padding: 15px;
+			background-color: #fff;
+		}
+	
+		.segmented-control {
+			margin-bottom: 15px;
+		}
+	
+		.button-group {
+			margin-top: 15px;
+			display: flex;
+			justify-content: space-around;
+		}
+	
+		.form-item {
+			display: flex;
+			align-items: center;
+		}
+	
+		.button {
+			display: flex;
+			align-items: center;
+			height: 35px;
+			margin-left: 10px;
+		}
+	.demand-info-item {
+		padding: 20upx 0;
+		border-bottom: 1upx solid #EEEEEE;
+	}
+
+	.demand-info-item>view {
+		color: #AAAAAA;
+		font-size: 16upx;
+	}
+
+	.demand-info-item>view:first-child {
+		color: #333333;
+		font-size: 20upx;
+		padding: 15upx 0;
+	}
+	.order-info-detail-receive {
+		color:#0062CC;
+		font-size: 40upx;
+		margin-top:40rpx;
+		margin-left: 20rpx;
+	}
+	.order-info-detail-refuse {
+		color: #CD1225;	
+		font-size: 40upx;
+		margin-top: 40rpx;
+		margin-left: 20rpx;
+		}
+	.order-info-detail-Complete {
+		color: #01B90B;	
+		font-size: 40upx;
+		margin-top: 25rpx;
+		margin-left: 20rpx;
+		}
+</style>
