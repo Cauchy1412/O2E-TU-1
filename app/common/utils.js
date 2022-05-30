57dbf69ecc4ceb696b45071f6884672629dd7b4b
karin0 <karin0@gmx.com>
2022-05-30 19:19:56 +0800

    app: fix date format for android

--- app/common/utils.js
+++ app/common/utils.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
 
-Vue.prototype.formatDate = date => {
-  return new Date(date).toLocaleString('zh-CN');
+Vue.prototype.formatDate = d => {
+	function pad(x) {
+		if (x < 10)
+			return '0' + x;
+		return x;
+	}
+	const date = new Date(d);
+	// return date.toLocaleString('zh-CN');
+	var seperator1 = "/";
+	var seperator2 = ":";
+	var month = date.getMonth() + 1;    
+	var strDate = date.getDate();
+	var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate + "  " +
+		pad(date.getHours()) + seperator2 + pad(date.getMinutes()) + seperator2 + pad(date.getSeconds());    
+	return currentdate;
 }
 
 Vue.prototype.navigateToPage = path => {
