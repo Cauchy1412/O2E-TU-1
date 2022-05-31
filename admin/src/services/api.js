03c979c70dcaf59657e4ab84ae47cf0ce9fd3d78
karin0 <karin0@gmx.com>
2022-05-31 15:07:26 +0800

    admin: fix request base url

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -3,6 +3,8 @@ const API_PROXY_PREFIX='/api'
 const BASE_URL = process.env.NODE_ENV === 'production' ?
     'http://114.116.221.131/api' :
     API_PROXY_PREFIX
+const BASE_AXIOS_URL = 'http://114.116.221.131';
+
 // const BASE_URL = process.env.VUE_APP_API_BASE_URL
 module.exports = {
   BASE_URL,
